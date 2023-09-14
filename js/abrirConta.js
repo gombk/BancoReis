79c68906ad015d9cc1f2f2772c0666583a640040
gombk <gombk666@gmail.com>
2023-09-14 14:42:18 +0000

    Deploying to gh-pages from @ gombk/BancoReis@71d4dc404647bdf531f622a3805b05711784dd0e 🚀

---
+++ js/abrirConta.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $("#deposit").hide();
+});
+
+
+$("#depositCheckbox").change(function () {
+    if ($(this).prop('checked')) {
+        $("#deposit").show();
+    } else {
+        $("#deposit").hide();
+    }
+})
---
+++ js/site.js
@@ -0,0 +1,46 @@
+﻿async function deposito() {
+    let valorDeposito = $("#depositoValor").val();
+    let response;
+
+    await fetch(`/Home/Depositar?valor=${valorDeposito}`)
+        .then(async (r) => response = await r.json())
+        .then(console.log(response))
+
+    location.reload(true)
+}
+
+async function saque() {
+    let valorSaque = $("#saqueValor").val();
+    let response;
+
+    await fetch(`/Home/Saque?valor=${valorSaque}`)
+        .then(async (r) => response = await r.json())
+
+    console.log(response)
+
+    location.reload(true)
+}
+
+async function emprestimo() {
+    let valorEmprestimo = $("#emprestimoValor").val();
+    let response;
+
+    await fetch(`/Home/Emprestimo?valor=${valorEmprestimo}`)
+        .then(async (r) => response = await r.json())
+
+    console.log(response);
+
+    location.reload(true);
+}
+
+async function pagarEmprestimo() {
+    let valorEmprestimo = $("#emprestimoRestante").val();
+    let response;
+
+    await fetch(`/Home/PagarEmprestimo?valor=${valorEmprestimo.replace(",", ".")}`)
+        .then(async (r) => response = await r.json())
+
+    console.log(response);
+
+    location.reload(true);
+}
